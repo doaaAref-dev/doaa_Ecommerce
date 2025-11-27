b364fccc9bb0d90864ce859ca1f29be1b8091254
doaa <doaaaref678@gmail.com>
2025-11-27 11:02:21 +0200

    Enable compact JSON parsing in index.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
-,
+  baseURL: import.meta.env.VITE_API_URL
 });
+
 
 // كل ريكوست هيضيف التوكن لو موجود
 api.interceptors.request.use((config) => {
